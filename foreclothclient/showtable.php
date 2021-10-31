2b3dfed2c35590f5ec5d0afdbc57e97d724c5b56
optima2315 <93474385+optima2315@users.noreply.github.com>
2021-10-31 14:04:30 +0200

    Add files via upload

---
+++ foreclothclient/showtable.php
@@ -0,0 +1,17 @@
+<?php
+
+$dsn = 'mysql:host=localhost; dbname=forecast';
+$pdo = new PDO($dsn, 'root', 'root');
+                                                        //Подключение создано
+
+$sql = "SELECT * FROM `".$_REQUEST['table']."`";        //Получаем все записи из выбранной таблицы
+$query = $pdo->prepare($sql);
+$query->execute();
+$row=[];
+
+do                                                      //Сохраняем в массив данные
+{$row[count($row)] = $query->fetch(PDO::FETCH_OBJ);
+if (!$row[count($row)-1]) {unset($row[count($row)-1]);break;}
+}
+while (true);
+echo json_encode($row);                                 //Перевод в формат json
